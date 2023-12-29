bf402c541cad17e3c3b46d0cdd918f6afaee5e82
Sosnin Andrey <KamushekKamushek@gmail.com>
2023-12-29 12:08:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState, useRef } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState('')
+	const [surname, setSurname] = useState('')
+
+	const refName = useRef(null)
+	const refSurname = useRef(null)
+	const refButton = useRef(null)
+
+	const handleInputName = (e) => {
+		if (e.key === 'Enter') {
+			if (!surname) {
+				refSurname.current.focus()
+			} else {
+				refButton.current.focus()
+			}
+		}
+	}
+	const handleInputSurname = (e) => {
+		if (e.key === 'Enter') {
+			if (!name) {
+				refName.current.focus()
+			} else {
+				refButton.current.focus()
+			}
+		}
+	}
+	return (
+		<div className='centr-centr'>
+			<input
+				type='text'
+				placeholder='Имя'
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+				ref={refName}
+				onKeyUp={handleInputName}
+			/>
+			<br />
+			<input
+				type='text'
+				placeholder='Фамилия'
+				value={surname}
+				onChange={(e) => setSurname(e.target.value)}
+				ref={refSurname}
+				onKeyUp={handleInputSurname}
+			/>
+			<br />
+			<button ref={refButton}>Перейти</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
