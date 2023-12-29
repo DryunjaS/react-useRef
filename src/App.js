bbd4288fb4e67cbc5b3d16cae2864b980ff08164
Sosnin Andrey <KamushekKamushek@gmail.com>
2023-12-29 22:57:39 +0300

    useMemo/memo

--- src/App.js
+++ src/App.js
@@ -1,53 +1,29 @@
 import './App.css'
-import React, { useState, useRef } from 'react'
+import React, { useState, memo } from 'react'
 
-function App() {
-	const [name, setName] = useState('')
-	const [surname, setSurname] = useState('')
-
-	const refName = useRef(null)
-	const refSurname = useRef(null)
-	const refButton = useRef(null)
-
-	const handleInputName = (e) => {
-		if (e.key === 'Enter') {
-			if (!surname) {
-				refSurname.current.focus()
-			} else {
-				refButton.current.focus()
-			}
-		}
-	}
-	const handleInputSurname = (e) => {
-		if (e.key === 'Enter') {
-			if (!name) {
-				refName.current.focus()
-			} else {
-				refButton.current.focus()
-			}
-		}
-	}
+const Table = memo(({ data }) => {
 	return (
-		<div className='centr-centr'>
-			<input
-				type='text'
-				placeholder='Имя'
-				value={name}
-				onChange={(e) => setName(e.target.value)}
-				ref={refName}
-				onKeyUp={handleInputName}
-			/>
-			<br />
-			<input
-				type='text'
-				placeholder='Фамилия'
-				value={surname}
-				onChange={(e) => setSurname(e.target.value)}
-				ref={refSurname}
-				onKeyUp={handleInputSurname}
-			/>
-			<br />
-			<button ref={refButton}>Перейти</button>
+		<table>
+			{data.map((item, id) => {
+				return (
+					<tr>
+						<td>{id}</td>
+						<td>{item}</td>
+					</tr>
+				)
+			})}
+		</table>
+	)
+})
+function App() {
+	const [data, setData] = useState([10, 20, 30, 40])
+
+	return (
+		<div className='App'>
+			<Table data={data} />
+			<button onClick={() => setData([...data, data[data.length - 1] + 10])}>
+				Добавить элемент
+			</button>
 		</div>
 	)
 }
